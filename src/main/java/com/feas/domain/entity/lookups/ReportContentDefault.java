a5fb2f39d3dfecd1303109e2ec546b79311522cb
Administrator <abrar.fci@gmail.com>
2024-02-12 10:27:10 +0300

    test

---
+++ src/main/java/com/feas/domain/entity/lookups/ReportContentDefault.java
@@ -0,0 +1,37 @@
+package com.feas.domain.entity.lookups;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="REPORT_CONTENT_DEFULT")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class ReportContentDefault implements Serializable {
+
+
+    @Column(name="ORDER_NR")
+    private BigDecimal orderNo;
+
+    @Column(name="NUMBER_AR")
+    private String numberAr;
+
+    @Column(name="DETAIL_TX")
+    private String detailTx;
+
+
+
+    @Column(name="CONTENT_ID")
+    private BigDecimal contentId;
+
+
+}
+
+
